f02a4c0cab9a91dd0a32ba4975f5b06a17445cd6
theshy1235 <2302164894@qq.com>
2023-10-27 23:09:16 +0800

    javaScript中的作用域

---
+++ js/scope/2.js
@@ -0,0 +1,17 @@
+//var a //声明提升
+//console.log(a);
+//a = 1
+
+//function foo() {
+//    console.log(123);
+//}
+//foo()
+
+//let  a = 1;
+//let  a = 2
+const a = 1  //常量
+a = 'hello'
+
+console.log(a);
+
+
---
+++ js/scope/3.js
@@ -0,0 +1,11 @@
+//if(1) {
+//  let a = 1
+//}
+//console.log(a);
+
+let a = 1
+if (true) {
+    console.log(a);//暂时性死区
+    let a = 2
+   
+}
