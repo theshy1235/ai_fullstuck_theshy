ce4dd3598d6fd9683af145e45b7a5e412b486d32
theshy1235 <2302164894@qq.com>
2023-11-16 21:19:14 +0800

    this

--- js/this/5.js
+++ js/this/5.js
@@ -1,15 +1,29 @@
-// function foo(){}
-// var foo = function(){}
+// function foo() {}
+
+// var foo = function() {}
+
+// var obj = {
+//     a: 1
+// }
+
+// var foo = () => {
+//     console.log(this.a);
+// }
+// foo.call(obj)
+
+
+// var bar = function() {
+//     console.log(this.a);
+// }
+// bar.call(obj)
 
 var obj = {
-    a:1
+    a: 1
 }
-var foo = () => {
-    console.log(this.a);
+function foo() {
+    var bar = () => {
+        console.log(this);
+    }
+    bar()
 }
-
-foo()
-
-var bar = function(){
-    c
-}
+foo.call(Obj)
