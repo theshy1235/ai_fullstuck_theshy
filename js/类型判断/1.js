f0a99ba5d361ca7596678d46e009e655cb4238a8
theshy1235 <2302164894@qq.com>
2023-11-23 23:22:36 +0800

    js的类型判断

--- js/类型判断/1.js
+++ js/类型判断/1.js
@@ -1,13 +1,21 @@
-let a = 123
-let b = 'hello world'
-let c = true
-let u = undefined
-let n = null
-let s = Symbol(123)
-let big = 123n
+// let a = 123
+// let b = 'hello world'
+// let c = true
+// let u = undefined
+// let n = null
+// let s = Symbol(123)//创建两个独一无二的值
+// let big = 123n
 
 
 
-let s1 = Symbol('hello')
-let s2 = Symbol('hello')
-console.log(s1 === s2)
+// let s1 = Symbol('hello')
+// let s2 = Symbol('hello')
+// console.log(s1 === s2)
+
+// let name = 'HELLO_WORLD'
+
+
+let obj = {}
+let fn = function() {}
+let arr = []
+let date = new Date()//特殊的对象
