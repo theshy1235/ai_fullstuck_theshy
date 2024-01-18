bf863c8f0f414532bb1ceb2f71cb29499a0f28f0
theshy1235 <2302164894@qq.com>
2024-01-18 23:42:07 +0800

    uniapp

---
+++ uniup/base/base/App.vue
@@ -0,0 +1,22 @@
+<script>
+	export default {
+		globalData: {
+			name: '全局的名称'
+		},
+		onLaunch: function(e) {
+			console.log('App Launch', e)
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style>
+	/*每个页面公共css */
+	/* @import 'common/font/iconfont.css'; */
+	/* @import url(https://at.alicdn.com/t/c/font_4416312_4qzw5cy6h0q.css); */
+</style>
